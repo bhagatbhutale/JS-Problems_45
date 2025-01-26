d6768b54acca0b6f83fe1d04ee473eb39c043141
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-01-26 23:35:34 +0530

    Q.1 Scope Related isCompleted

---
+++ one.js
@@ -0,0 +1,10 @@
+// Q.1 :- Find Code output (with reason)
+var x = Math.floor(Math.random());
+if (x > 0.5) {
+  var x = 1;
+} else {
+  var x = 2;
+}
+
+// console.log(Math.floor(0.99));
+console.log(x);
