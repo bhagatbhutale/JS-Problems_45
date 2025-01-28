37a5f52084e6821430f21c985de9cc2f2c860871
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-01-28 23:22:33 +0530

    Solving a 7 and 8 Questions

---
+++ eight.js
@@ -0,0 +1,14 @@
+// Q.8:- Find last element of an array ?
+// ( Withour using Length property )
+
+const arr = ["White", "Black", "red", "yellow"];
+console.log("??????????");
+
+// The at() method takes an integer value.
+// Returns the item at that index
+// allow positive & Negative integer :- (-1)
+
+console.log(arr.at(-1));
+arr.push("Gray;");
+
+console.log(arr.at(-1));
---
+++ seven.js
@@ -0,0 +1,10 @@
+// GIven array Find the Output of the Code ?
+// 1.
+const strFrom = Array.from("testform");
+console.log(strFrom);
+// 2.
+const arrFrom = Array.from([1, 2, 3], (x) => x + x);
+console.log(arrFrom);
+// 3.
+const result = Array.from({ length: 10 }, (a, b) => b);
+console.log(result);
